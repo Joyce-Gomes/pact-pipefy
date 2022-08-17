a7d18f7c4fc80e28cfa3dbaaf18b3a90063363f8
unknown <joy.gomessantos@gmail.com>
2022-08-16 21:22:01 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+export default function Header() {
+  return (
+    <Container>
+      <h1>Pact Pipefy</h1>
+    </Container>
+  )
+}
